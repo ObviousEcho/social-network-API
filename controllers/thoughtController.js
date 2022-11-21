f805f03d42c3cdad68d76f98ac45741cb0c50c6f
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-11-20 23:25:51 -0700

    created CRUD routes for thoughtController

---
+++ controllers/thoughtController.js
@@ -0,0 +1,61 @@
+const Thought = require("../models/Thought");
+
+module.exports = {
+  getThoughts(req, res) {
+    Thought.find().then(async (thoughts) => {
+      const thoughtObj = {
+        thoughts,
+      };
+      return res.json(thoughtObj);
+    });
+  },
+
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then(async (thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that id" })
+          : res.json({
+              thought,
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(400).json({ message: "No thought with that id" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No such thought exists" })
+          : res.json({ message: "Thought successfully deleted!" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
